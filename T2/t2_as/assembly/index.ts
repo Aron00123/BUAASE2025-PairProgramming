546eee03be8d61b3107a6e709d8043798d802667
TripleCamera <TripleCamera@outlook.com>
2025-03-28 22:13:50 +0800

    2025-03-28

--- T2/t2_as/assembly/index.ts
+++ T2/t2_as/assembly/index.ts
@@ -1,5 +1,159 @@
 // The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+class Queue<T> {
+  private data: T[] = []
+  private head: i32 = 0
+
+  push(value: T): void {
+    this.data.push(value)
+  }
+
+  pop(): T {
+    const value = this.data[this.head]
+    this.head++
+    return value
+  }
+
+  empty(): bool {
+    return this.head >= this.data.length
+  }
+}
+
+enum Legend {
+  CBORDER = '+'.codePointAt(0),
+  HBORDER = '-'.codePointAt(0),
+  VBORDER = '|'.codePointAt(0),
+  BARRIER = '#'.codePointAt(0),
+  SPACE = ' '.codePointAt(0),
+  FRUIT = '*'.codePointAt(0),
+  SNAKE4 = '4'.codePointAt(0),
+  SNAKE1 = '1'.codePointAt(0),
+  SNAKE2 = '2'.codePointAt(0),
+  SNAKE3 = '3'.codePointAt(0),
+}
+
+/**
+ *  1st dim.        y
+ *  |               ^
+ *  |               |
+ *  |               |
+ *  v               |
+ *   ---> 2nd dim.  +---> x
+ * 0: UP (y+)
+ * 1: LEFT (x-)
+ * 2: DOWN (y-)
+ * 3: RIGHT (x+)
+ */
+const DX = [-1, 0, 1, 0]
+const DY = [0, -1, 0, 1]
+const INF = 100
+
+function printMap(map: i32[][]): void {
+  for (let i = 0; i < map.length; i++) {
+    let row = ''
+    for (let j = 0; j < map[i].length; j++) {
+      row += String.fromCharCode(map[i][j])
+    }
+    console.log(row)
+  }
+}
+
+function printDepth(depth: i32[][]): void {
+  for (let i = 0; i < depth.length; i++) {
+    console.log(depth[i].toString())
+  }
+}
+
+function canMove(map: i32[][], x: i32, y: i32, d: i32): bool {
+  switch (map[x][y]) {
+    // Border
+    case Legend.CBORDER:
+    case Legend.HBORDER:
+    case Legend.VBORDER:
+    // Barrier
+    case Legend.BARRIER:
+      return false
+    // Space
+    case Legend.SPACE:
+    // Fruit
+    case Legend.FRUIT:
+      return true
+    // Snake
+    case Legend.SNAKE4:
+      return true
+    case Legend.SNAKE3:
+      return d >= 2
+    case Legend.SNAKE2:
+      return d >= 3
+    case Legend.SNAKE1:
+      return d >= 4
+    default:
+      throw new Error('Invalid map')
+  }
+}
+
+export function greedySnakeMoveBarriers(snake: i32[], fruit: i32[], barrier: i32[]): i32 {
+  const map = new Array<i32[]>(10)
+  for (let i = 0; i < 10; i++) {
+    map[i] = new Array<i32>(10).fill(Legend.SPACE)
+  }
+  map[0][0] = map[0][9] = map[9][0] = map[9][9] = Legend.CBORDER
+  for (let i = 1; i < 9; i++) {
+    map[0][i] = map[9][i] = Legend.HBORDER
+    map[i][0] = map[i][9] = Legend.VBORDER
+  }
+  for (let i = 0; i < 4; i++) {
+    const snake_x = snake[i * 2]
+    const snake_y = snake[i * 2 + 1]
+    map[snake_x][snake_y] = '0'.charCodeAt(0) + (i + 1)
+    console.log(`snake ${i+1}: ${snake_x} ${snake_y}`)
+  }
+  const fruit_x = fruit[0]
+  const fruit_y = fruit[1]
+  map[fruit_x][fruit_y] = Legend.FRUIT
+  for (let i = 0; i < 12; i++) {
+    const barrier_x = barrier[i * 2]
+    const barrier_y = barrier[i * 2 + 1]
+    map[barrier_x][barrier_y] = Legend.BARRIER
+  }
+  printMap(map)
+
+  const depth = new Array<i32[]>(10)
+  for (let i = 0; i < 10; i++) {
+    depth[i] = new Array<i32>(10).fill(INF)
+  }
+  depth[snake[0]][snake[1]] = 0
+
+  const q = new Queue<i32[]>()
+  q.push([snake[0], snake[1], 0])
+  while (!q.empty()) {
+    const item = q.pop()
+    const x = item[0]
+    const y = item[1]
+    const d = item[2]
+    if (x === fruit_x && y === fruit_y) {
+      break
+    }
+    for (let i = 0; i < 4; i++) {
+      const next_x = x + DX[i]
+      const next_y = y + DY[i]
+      if (depth[next_x][next_y] > d + 1 && canMove(map, next_x, next_y, d + 1)) {
+        depth[next_x][next_y] = d + 1
+        q.push([next_x, next_y, d + 1])
+      }
+    }
+  }
+  // printDepth(depth)
+  if (depth[fruit_x][fruit_y] === INF) {
+    return -1
+  }
+  for (let i = 0; i < 4; i++) {
+    const next_x = snake[0] + DX[i]
+    const next_y = snake[1] + DY[i]
+    if (canMove(map, next_x, next_y, 1)) {
+      console.log(next_x.toString() + ' ' + next_y.toString() + ' ' + i.toString())
+      return i
+    }
+  }
+  throw new Error('Invalid depth')
 }
--- T2/t2_as/tests/index.js
+++ T2/t2_as/tests/index.js
@@ -1,4 +1,79 @@
 import assert from "assert";
-import { add } from "../build/debug.js";
-assert.strictEqual(add(1, 2), 3);
-console.log("ok");
+import { greedySnakeMoveBarriers } from "../build/debug.js";
+
+function test(map, expected) {
+  const rows = map.trim().split("\n")
+  const snake = new Array(8).fill(-1)
+  const fruit = []
+  const barrier = []
+  for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < rows[i].length; j++) {
+      const ch = rows[i].charAt(j)
+      if (ch === '1' || ch === '2' || ch === '3' || ch === '4') {
+        const pos = ch.codePointAt(0) - '1'.codePointAt(0); // 0 <= pos <= 3
+        snake[pos * 2] = i;
+        snake[pos * 2 + 1] = j;
+      } else if (ch === "*") {
+        fruit.push(i);
+        fruit.push(j);
+      } else if (ch === "#") {
+        barrier.push(i);
+        barrier.push(j);
+      }
+    }
+  }
+  const actual = greedySnakeMoveBarriers(snake, fruit, barrier)
+  assert.ok(expected.includes(actual), `Expected ${expected}, got ${actual}`)
+}
+
+test(`
++--------+
+|*  #    |
+|    #   |
+|   #####|
+|   #    |
+|   #   1|
+|   #   2|
+|   #   3|
+|   #   4|
++--------+
+`, [-1]);
+
+test(`
++--------+
+|   #    |
+|  #*#   |
+|   #    |
+|   #    |
+|   #   1|
+|   #   2|
+|   #   3|
+|####   4|
++--------+
+`, [-1]);
+
+test(`
++--------+
+|*       |
+|      ##|
+|     ###|
+|    ####|
+|    ###1|
+|       2|
+|       3|
+|       4|
++--------+
+`, [-1]);
+
+test(`
++--------+
+|*    ###|
+|     ###|
+|     ###|
+|        |
+|        |
+|      ##|
+|     #14|
+|      23|
++--------+
+`, [3]);
